0925d61a313179271001b24eebac049bcb1da379
irbingestudios-sys <irbing.estudios@gmail.com>
2025-11-05 11:07:28 -0500

    Update supabaseClient.js

--- src/js/supabaseClient.js
+++ src/js/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
+export const supabase = createClient(
+  'https://qeqltwrkubtyrmgvgaai.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' // clave pública anon
+);
