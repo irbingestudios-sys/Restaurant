82ad630dcecffe581591171ad8a0a1107415df2a
irbingestudios-sys <irbing.estudios@gmail.com>
2025-11-16 21:15:30 -0500

    Update cocina.js

--- src/js/cocina.js
+++ src/js/cocina.js
@@ -8,7 +8,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // 🔐 Conexión Supabase con sesión activa
 const supabase = createClient(
   "https://qeqltwrkubtyrmgvgaai.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." // clave pública
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlcWx0d3JrdWJ0eXJtZ3ZnYWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMjY1MjMsImV4cCI6MjA3NzgwMjUyM30.Yfdjj6IT0KqZqOtDfWxytN4lsK2KOBhIAtFEfBaVRAw" // clave pública
 );
 
 window.supabase = supabase;
